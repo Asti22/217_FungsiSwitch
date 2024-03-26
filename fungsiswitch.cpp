48fd5f17e5786bcc2da936609b0cbaa00e1ba87d
Asti22 <uun3461@gmail.com>
2024-03-26 14:48:42 +0700

    menambah proses perkalian

--- fungsiswitch.cpp
+++ fungsiswitch.cpp
@@ -27,3 +27,23 @@ float pembagian(int a,int b)
     return a / b;
 }
 
+int main(){
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator sederhana" << endl;
+        cout << "======================" << endl;
+        cout << "1. Penjumalahan" << endl;
+        cout << "2. Pengurangan " << endl;
+        cout << "3. Perkalian " << endl;
+        cout << "4. Pembagian "<< endl;
+        cout << "5. Keluar" <<endl;
+        cout <<"================" << endl;
+        cin >>pilihan;
+        
+        switch (pilihan)
+        /* code */
+    } while (/* condition */);
+    
+}
+
