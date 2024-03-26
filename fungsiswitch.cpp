87731df303c30901ea2be8b26523aa13541a38f4
Asti22 <uun3461@gmail.com>
2024-03-26 14:57:06 +0700

    menambahkan pilihan case 2 pengurangan

--- fungsiswitch.cpp
+++ fungsiswitch.cpp
@@ -47,6 +47,10 @@ int main(){
          inputData();
          cout <<"Hasil Penjumlahan =" <<penjumlahan(bilangan1, bilangan2) <<endl;
          break;
+         case 2:
+         inputData();
+         cout <<"Hasil pengurangan =" <<pengurangan(bilangan1, bilangan2) <<endl;
+         break;
         /* code */
     } while (/* condition */);
     
