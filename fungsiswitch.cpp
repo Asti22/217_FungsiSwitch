52417a4fe0b640408258cf3809d38ae83e2bbacb
Asti22 <uun3461@gmail.com>
2024-03-26 14:42:06 +0700

    menambah proses pengurangan

--- fungsiswitch.cpp
+++ fungsiswitch.cpp
@@ -14,4 +14,16 @@ int penjumlahan(int a, int b)
 {
     return a + b;
 }
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+int perkalian(int a,int b)
+{
+    return a * b;
+}
+float pembagian(int a,int b)
+{
+    return a / b;
+}
 
